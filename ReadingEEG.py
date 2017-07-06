56ac48daf91e9b5d422bfd4d5eecb269b70b936b
tonydong6 <tdong6@gmail.com>
2017-07-06 16:38:20 -0400

    Added autocorrelation modeling with Yule-Walker

--- ReadingEEG.py
+++ ReadingEEG.py
@@ -8,8 +8,10 @@ Created on Thu Jun 22 12:41:36 2017
 'Import functions'
 import numpy as np
 import mne
-from matplotlib import pyplot as plot
+from matplotlib import pyplot as plt
 from mne.preprocessing import ICA, create_ecg_epochs
+import spectrum
+from pylab import *
 
 
 'Import file (this is a .set file)'
@@ -33,7 +35,6 @@ data1 = data1.T
 (timepoints, channels) = data1.shape
 frequency = raw.info['sfreq']
 
-
 'ICA'
 
 ica = ICA(n_components=64)#set up ica
@@ -44,10 +45,31 @@ ica.plot_components() #plot the topographs of each component
 
 Montage.plot(show_names=True) #plot the electrode locations
 
-ica.plot_properties(raw, picks=4)#plot the properties of each single component
+ica.plot_properties(raw, picks=35)#plot the properties of each single component
 
 ica.plot_sources(raw)#plot the timecourse of each component
 
 ica.plot_overlay(raw, exclude=[1,2,3]) #plot the proposed transformation
 
-ica.apply(raw,exclude=[1,2,3]) #apply the transformation
+ica.apply(raw,exclude=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64]) #apply the transformation
+
+'autoregression'
+
+(newdata, newtimes) = raw[::,::]
+channel1data = newdata[49,::]
+last1second = channel1data[-512*2:-512]
+
+[ar, var, reflec] = spectrum.aryule(last1second, 511, norm= 'biased')
+psd = spectrum.arma2psd(ar)
+
+noise = randn(1, 1024)
+y = lfilter([1], ar, noise); 
+
+ps = np.abs(np.fft.fft(channel1data))**2
+time_step = 1 / 512
+freqs = np.fft.fftfreq(last1second.size, time_step)
+idx = np.argsort(freqs)
+
+plt.plot(freqs, ps)
+axis([0, 2, 0, 100000])
+
